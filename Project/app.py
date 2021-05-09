951ebb8e3176506e1ff21c77edf98c2f0bb4fa7b
Riki <riki@Riki-MacBook-Pro.local>
2021-05-09 23:50:52 +0800

    manage_page

--- Project/app.py
+++ Project/app.py
@@ -17,16 +17,73 @@ app = create_app()
 # init blueprint
 app.register_blueprint(auth_app)
 
-app.add_url_rule('/', endpoint='index')
+app.add_url_rule('/', endpoint='manage_login')
 
 # route
 @app.route('/hello')
 def hello():
     return 'Hello flask!'
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+
+@app.route('/manage_index')
+def manage_index():
+    return render_template('manage_index.html')
+
+
+@app.route('/manage_login')
+def manage_login():
+    return render_template('manage_login_register.html')
+
+
+@app.route('/manage_route_form')
+def manage_route_form():
+    return render_template('manage_route_form.html')
+
+
+@app.route('/manage_route_table')
+def manage_route_table():
+    return render_template('manage_route_table.html')
+
+
+@app.route('/manage_station_form')
+def manage_station_form():
+    return render_template('manage_station_form.html')
+
+
+@app.route('/manage_station_table')
+def manage_station_table():
+    return render_template('manage_station_table.html')
+
+
+@app.route('/manage_ticket_form')
+def manage_ticket_form():
+    return render_template('manage_ticket_form.html')
+
+
+@app.route('/manage_ticket_table')
+def manage_ticket_table():
+    return render_template('manage_ticket_table.html')
+
+
+@app.route('/manage_train_form')
+def manage_train_form():
+    return render_template('manage_train_form.html')
+
+
+@app.route('/manage_train_table')
+def manage_train_table():
+    return render_template('manage_train_table.html')
+
+
+@app.route('/manage_user_form')
+def manage_user_form():
+    return render_template('manage_user_form.html')
+
+
+@app.route('/manage_user_table')
+def manage_user_table():
+    return render_template('manage_user_table.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
