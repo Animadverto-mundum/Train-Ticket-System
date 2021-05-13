97e1b1275e63f63212ac7e8ddf5f1f2a958d5f2e
Doctor <excogitatoris_doctor@outlook.com>
2021-05-13 10:37:10 +0800

    train table

--- Project/Manager/train.py
+++ Project/Manager/train.py
@@ -1,13 +1,13 @@
 from flask import redirect, render_template
 from . import manager_bp
-from model import db, TrainNumber
+from model import db, TrainNumber, Line
 
 @manager_bp.route('/train_view')
 def train_view():
-    lines = db.session.query.filter().all()
-    lists = db.session.query.filter().all()
+    trains = db.session.query(TrainNumber).filter().all()
+    lists = db.session.query(Line).filter().all()
     dict = {item.line_ID: item.line_name for item in lists}
-    return render_template('manage_train_table.html', {'lines': lines, 'dict': dict})
+    return render_template('manage_train_table.html', trains = trains, dict = dict)
 
 @manager_bp.route('/train_edit')
 def train_edit():
