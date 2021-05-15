211d680669dadfcbee4619a8b014a643f9516158
Doctor <excogitatoris_doctor@outlook.com>
2021-05-15 11:01:48 +0800

    ticket view with delete

--- Project/Manager/ticket.py
+++ Project/Manager/ticket.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, request, redirect, url_for
 from . import manager_bp, access_check
 from model import db, TrainNumber, Line, TicketsSold, FareInformation, User
 
-@manager_bp.route('/ticket_view', methods = ['GET', 'POST'])
+@manager_bp.route('/ticket_view', methods = ['GET'])
 def ticket_view():
     if not access_check(request, 0):
         response = redirect(url_for('manager_bp.manager_index'))
@@ -19,6 +19,19 @@ def ticket_view():
         'user_name': request.cookies.get('user_name'),
         }
     return render_template('manage_ticket_table.html', **render_args)
+
+@manager_bp.route('/ticket_delete', methods = ['GET'])
+def ticket_delete():
+    if not access_check(request, 0):
+        response = redirect(url_for('manager_bp.manager_index'))
+        response.delete_cookie('user_name')
+        response.delete_cookie('user_type')
+        return response
+    delete_tickets_sold_ID = request.args.get('delete_tickets_sold_ID')
+    delete_item = db.session.query(TicketsSold).filter(TicketsSold.tickets_sold_ID == delete_tickets_sold_ID).first()
+    db.session.delete(delete_item)
+    db.session.commit()
+    return redirect(url_for('manager_bp.ticket_view'))
 
 @manager_bp.route('/ticket_edit')
 def ticket_edit():
