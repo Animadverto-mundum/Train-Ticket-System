6c6cfe58b3f60152a9baa734ff0fbb5cad568691
Rrilin <690895287@qq.com>
2021-05-23 14:09:40 +0800

    用户端美化完毕+手机端登出键

--- Project/app.py
+++ Project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, json, Response, redirect, url_for, session, render_template
+from flask import Flask, redirect, url_for
 from model import db
 from Manager import manager_bp
 from User import user_bp
@@ -9,11 +9,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)  # 配置文件
     db.init_app(app)
-
     # init blueprint
     app.register_blueprint(manager_bp)
     app.register_blueprint(user_bp)
-
     return app
 
 
@@ -25,5 +23,6 @@ app = create_app()
 def hello():
     return redirect(url_for('user_bp.user_index'))
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8088)
+    app.run(debug=True, port=9000)
